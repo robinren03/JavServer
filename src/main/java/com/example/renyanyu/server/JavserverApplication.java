66afb0ac3a7c0f891e65a02c90fcdd001582e922
Ren <ryy19@mails.tsinghua.edu.cn>
2021-09-04 21:27:46 +0800

    Successfully implement basic list

--- src/main/java/com/example/renyanyu/server/JavserverApplication.java
+++ src/main/java/com/example/renyanyu/server/JavserverApplication.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import joinery.DataFrame;
+import joinery.DataFrame.Predicate;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -33,13 +33,20 @@ public class JavserverApplication {
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Object> getList(
-			@RequestParam(value = "course", required = true) String course)
+			@RequestParam(value = "course", required = true) String course,
+			@RequestParam(value = "page", required = true)int page)
 	{
-		Resource resource = new ClassPathResource(course+".csv");
+		Resource resource = new ClassPathResource("./"+ course + ".csv");
 		try {
 			InputStream is = resource.getInputStream();
 			DataFrame<Object> dt = DataFrame.readCsv(is);
-			return dt.col(3);
+			List<Object> ls = dt.select(new Predicate<Object>() {
+				@Override
+				public Boolean apply(List<Object> values) {
+					return String.class.cast(values.get(1)).equals("http://www.w3.org/2000/01/rdf-schema#label");
+				}
+			}).slice(page*15, page*15 + 14).col(0);
+			return ls;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
